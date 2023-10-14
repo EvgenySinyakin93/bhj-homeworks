12b41e40c5eb3ffb0a183a25ecaab7600a216acb
Zhenya Sinyakin <evgeny.siniakin@yandex.ru>
2023-10-14 11:26:11 +0300

    ДЗ готово не полностью

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,18 +1,21 @@
-//Раскрываем список
-function myFunction(){
-    document.getElementsByClassName('dropdown__list').classList.toggle('dropdown__list_active');
-}
+//сверни разверни 
 
-//Закрыватие
-window.onclick = function(event) {
-    if (!event.target.matches('.dropdown__value')) {
-        const drop = document.getElementsByClassName('dropdown__list');
-        let i;
-        for (let i = 0; i < drop.length; i++) {
-            const openDrop = drop[i];
-            if (openDrop.classList.contains('dropdown__list_active')) {
-                openDrop.classList.remove('dropdown__list_active');
-            }
-        }
-    }
-}
+const button = document.getElementById("button");
+const listItems = document.querySelectorAll(".dropdown__list");
+
+button.addEventListener("click", () => {
+  for (let i = 0; i < listItems.length; i++) {
+    listItems[i].classList.toggle("dropdown__list_active");
+  }
+});
+
+//замена пункта при выборе
+document.querySelectorAll(".dropdown__item").forEach(item => {
+    item.addEventListener("click", function() {
+      const dropdown = this.closest(".dropdown");
+      const value = this.textContent;
+  
+      dropdown.querySelector(".dropdown__value").innerText = value;
+      dropdown.querySelector(".dropdown__item").style.display = "flex";
+        });
+  });
